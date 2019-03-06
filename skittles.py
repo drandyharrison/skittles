6f6fb4b25dd50146b59f70846596782fa5a3e5b2
Andy Harrison <iam.andyharrison@gmail.com>
2019-03-06 21:28:27 +0000

    Made code to read fixtures into a function - get_fixtures and added first bit of validation

--- skittles.py
+++ skittles.py
@@ -8,14 +8,18 @@ team = 'Victory Buoys'
 
 xlsx = XLSXhandler(xlsx_fname)
 
-# Read the skittles Excel
-try:
-    # load spreadsheet
+def get_fixtures(xlsx_fname, team):
+    """Read the skittles fixtures from Excel
+    * xlsx_fname - name of the Excel file containing the fixture information
+    * team - the team whose fixtures are to be read (= worksheet name)"""
+    # TODO validation
+    # TODO xlsx_fname is a non-empty string with the suffix *.xlsx
+    if not isinstance(xlsx_fname, str):
+        raise ValueError("@get_fixtures({}, {}) - {} is not a string".format(xlsx_fname, team, xlsx_fname))
+    # TODO team is a non-empty string
     if xlsx.get_xlsx_from_file():
         print(xlsx.get_sheet_names())
         # TODO get Victory Buoys fixtures
-except FileNotFoundError:
-    print("ERROR: file not found - {}".format(fname))
 
 # TODO write (future) fixtures to Google calendar
 
